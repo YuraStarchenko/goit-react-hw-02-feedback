b2aeb5ddc9dd9d37cdd669a470310141a6e57f48
YuraStarchenko <100568416+YuraStarchenko@users.noreply.github.com>
2023-03-22 15:00:35 +0200

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,19 @@
+import { Component } from 'react';
 import { GlobalStyle } from '../GlobalStyle';
 import { Container } from './Container.styled';
 
-export const App = () => {
-  return (
-    <Container>
-      <GlobalStyle />
-    </Container>
-  );
-};
- 
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+	};
+	
+  render() {
+    return (
+      <Container>
+        <GlobalStyle />
+      </Container>
+    );
+  }
+}
